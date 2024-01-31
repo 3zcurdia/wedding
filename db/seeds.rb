af50ef09b0018040c182a7b79f8fe93069b75380
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2024-01-30 18:45:27 -0600

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 #   end
 
 # Create a default admin user
-admin = Admin.find_or_create_by!(email: "admin@weddingsite.com") do |admin|
+AdminUser.find_or_create_by!(email: "admin@weddingsite.com") do |admin|
   admin.password = "Secret password jarden #22"
   admin.password_confirmation = "Secret password jarden #22"
 end
