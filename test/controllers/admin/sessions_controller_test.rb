e1e338447767744e43028609036dbdc06e3a8498
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2024-02-10 09:33:50 -0600

    Fix broken session controller tests

--- test/controllers/admin/sessions_controller_test.rb
+++ test/controllers/admin/sessions_controller_test.rb
@@ -8,26 +8,23 @@ class Admin::SessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    skip
-    admin_sign_in_as @admin
+    admin_sign_in
 
-    get sessions_url
+    get admin_sessions_url
 
     assert_response :success
   end
 
   test "should get new" do
-    skip
     get admin_sign_in_url
 
     assert_response :success
   end
 
   test "should sign in" do
-    skip
     post admin_sign_in_url, params: { email: @admin.email, password: "Secret1*3*5*" }
 
-    assert_redirected_to root_url
+    assert_redirected_to admin_url
 
     get root_url
 
@@ -35,27 +32,25 @@ class Admin::SessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should not sign in with wrong credentials" do
-    skip
     post admin_sign_in_url, params: { email: @admin.email, password: "SecretWrong1*3" }
 
     assert_redirected_to admin_sign_in_url(email_hint: @admin.email)
     assert_equal "That email or password is incorrect", flash[:alert]
 
-    get root_url
+    get admin_url
 
-    assert_redirected_to sign_in_url
+    assert_redirected_to admin_sign_in_url
   end
 
   test "should sign out" do
-    skip
-    admin_sign_in_as @admin
+    admin_sign_in
 
-    delete session_url(@admin.sessions.last)
+    delete admin_session_url(@admin.sessions.last)
 
-    assert_redirected_to sessions_url
+    assert_redirected_to admin_sessions_url
 
     follow_redirect!
 
-    assert_redirected_to sign_in_url
+    assert_redirected_to admin_sign_in_url
   end
 end
