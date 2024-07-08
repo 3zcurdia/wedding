b24aef692a4dd345939139049db5ca5763cc8808
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2024-07-07 22:38:24 -0600

    Fix linter issues

--- app/controllers/admin/guests/exports_controller.rb
+++ app/controllers/admin/guests/exports_controller.rb
@@ -5,7 +5,8 @@ module Admin
     class ExportsController < ApplicationController
       def show
         csv = Guest.export_csv(Guest.main.order_by_priority)
-        send_data csv, type: "text/csv", disposition: "attachment", filename: "guests-#{Time.now.strftime("%Y%m%d%H%M%S")}.csv"
+        send_data csv, type: "text/csv", disposition: "attachment",
+                       filename: "guests-#{Time.now.strftime("%Y%m%d%H%M%S")}.csv"
       end
     end
   end
